ab2782535c63e922c45494d84ead6a6375fa0f62
Gerrit Van Mol <gerrit.vanmol@student.odisee.be>
2022-04-24 13:15:14 +0000

    Add demo project and app folder for final product

--- demo-project/myweb/events/models.py
+++ demo-project/myweb/events/models.py
@@ -8,12 +8,24 @@ class Venue(models.Model):
     web = models.URLField('Website address')
     email_adress = models.EmailField('Email address')
 
-class Events(models.Model):
+    def __str__(self):
+        return self.name
+
+class MyClubUser(models.Model):
+    first_name =  models.CharField("User name", max_length=30)
+    last_name = models.CharField("User last name", max_length=50)
+    email = models.EmailField("User email", max_length=254)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Event(models.Model):
     name = models.CharField('Event name', max_length=120)
     event_date = models.DateTimeField('Event date')
-    venue = models.CharField()
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE) #cascade is used to remove all connected data when table or entry is removed
     manager = models.CharField('Manager name', max_length=120)
-    description = models.TextField(blank=true)
+    description = models.TextField(blank=True)
+    attendees = models.ManyToManyField(MyClubUser, blank=True)
 
     def __str__(self):
         return self.name
