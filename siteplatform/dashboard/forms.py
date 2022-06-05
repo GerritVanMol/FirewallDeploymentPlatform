34c9d712eef3117ebb0dc56e5089d6dd782a835b
Gerrit Van Mol <gerrit.vanmol@student.odisee.be>
2022-06-05 19:43:16 +0000

    update register functionality

--- siteplatform/dashboard/forms.py
+++ siteplatform/dashboard/forms.py
@@ -1,3 +1,4 @@
+from logging import PlaceHolder
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
@@ -5,6 +6,10 @@ from django.contrib.auth.models import User
 
 
 class CreateUserForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"class":"", "placeholder":"Username", "id":"username"}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={"class":"", "placeholder":"Email", "id":"email"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"", "placeholder":"Password", "id":"password1"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"", "placeholder":"Comfirm password", "id":"password2"}))
     class Meta:
         model = User;
         fields = ['username', 'email', 'password1', 'password2']
