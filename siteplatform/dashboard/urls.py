d1551a44303f5a870c9ee043cbb7cb2126ef1d62
Gerrit Van Mol <gerrit.vanmol@student.odisee.be>
2022-06-05 15:47:25 +0000

    make test user registration form

--- siteplatform/dashboard/urls.py
+++ siteplatform/dashboard/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.home, name="login"),
+    path('', views.loginPage, name="login"),
+    path('login', views.loginPage, name="login"),
+    path('register', views.registrationPage, name="register"),
 ]
--- siteplatform/dashboard/views.py
+++ siteplatform/dashboard/views.py
@@ -1,6 +1,20 @@
 from urllib import response
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.forms import inlineformset_factory
+from .forms import CreateUserForm
 
 # Create your views here.
-def home (request):
+def loginPage (request):
     return render(request, "login.html", { })
+
+
+def registrationPage (request):
+    form = CreateUserForm()#render form
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)#post form input
+        if form.is_valid():
+            form.save()
+
+    context = { "form":form }
+    return render(request, "register.html", context)
