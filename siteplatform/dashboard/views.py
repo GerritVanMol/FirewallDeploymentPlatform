03281189febb3b2fb5f05010b7d13b4f51b207be
Gerrit Van Mol <gerrit.vanmol@student.odisee.be>
2022-06-05 13:04:47 +0000

    update static files and templates

---
+++ siteplatform/dashboard/views.py
@@ -0,0 +1,6 @@
+from urllib import response
+from django.shortcuts import render
+
+# Create your views here.
+def home (request):
+    return render(request, "login.html", { })
--- siteplatform/siteplatform/settings.py
+++ siteplatform/siteplatform/settings.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import os
+import sys
 from decouple import config
 from decouple import Csv
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,6 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -28,8 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'accounts',
+    'corsheaders',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -40,6 +41,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'siteplatform.urls'
@@ -113,14 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static'),
 )
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
